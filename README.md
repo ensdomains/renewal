47061a3df471259e11e9f52fad9d67255452aee9
Makoto <inouemak@googlemail.com>
2020-04-08 17:39:14 +0100

    Add index.js

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ This js library will allow wallets and dapps to remind users that they own expir
 
 ## Usage
 
-
 ```js
 import { checkRenewal } from '@ensdomains/renewal'
 
@@ -13,19 +12,17 @@ const userAddress = '0x123...'
 const referrerAddress = '0x234...'
 
 const {
-  hasExpiringDomains,
   numExpiringDomains,
   renewalUrl
  } = await checkRenewal(userAddress, referrerAddress)
 
-if(hasExpiringDomains){
+if(hasExpiringDomains > 0){
   return <a href={renewalUrl}>You have {numExpiringDomains} names needing renewal!</a>
 }
 ```
 
 - `renewalUrl` = `https:/app.ens.domains/address/0x123/renewal?referrer=0x345`
-- `numExpiringDomains` return number or `more than 100` if it has more than 100 names.
-
+- `numExpiringDomains` return only up to 100
 
 ## Notes
 
---
+++ index.js
@@ -0,0 +1,46 @@
+import { request, GraphQLClient } from 'graphql-request'
+
+export const GET_DOMAINS_OWNED_BY_ADDRESS_FROM_SUBGRAPH = `
+  query getDomains($userAddress: String!, $expiryDate: Int!) {
+    account(id: $userAddress) {
+      registrations(
+        orderBy:expiryDate, orderDirection:asc,
+        where:{ expiryDate_lt: $expiryDate }
+      ) {
+        expiryDate
+        domain {
+          labelName
+        }
+      }
+    }
+  }
+`
+
+const endpoint = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
+const client = new GraphQLClient(endpoint, {
+  headers: {
+      Authorization: ``
+  }
+})
+
+export async function checkRenewal(userAddress, referrerAddress, {expiryDate, debug}) {
+  if(!expiryDate){
+    let date = new Date();
+    expiryDate = date.setDate(date.getDate() + 30);  
+  }else{
+    expiryDate = expiryDate.getTime();
+  }
+  const { account } = await client.request(GET_DOMAINS_OWNED_BY_ADDRESS_FROM_SUBGRAPH, {
+    userAddress: userAddress.toLowerCase(),
+    expiryDate: parseInt(expiryDate / 1000)
+  })
+  const count = account.registrations.length
+  if(debug){
+    console.log(account.registrations.map((r) => { return [r.domain.labelName, new Date(r.expiryDate * 1000)]}))
+  }
+  const res = {
+    numExpiringDomains: count,
+    renewalUrl: `https://app.ens.domains/address/${userAddress}?referrer=${referrerAddress}`
+  }
+  return res
+}
